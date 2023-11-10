a2d65afe79c65bcd82b5ea8334d17a4089db115c
DarjaCodes <krjukova4darja@gmail.com>
2023-11-10 12:19:23 +0100

    carousel-working

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,22 +1,29 @@
+import React, { useState } from 'react';
+import Slide1 from '../Carousel/img/2.png';
+import Slide2 from '../Carousel/img/3.png';
+
+const slides = [Slide1, Slide2];
+
 export const Carousel = () => {
+  const [index, setSlide] = useState(0);
+
   return (
-    <div id="carouselExample" className="carousel slide">
+    <div id="carousel" className="carousel slide">
       <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img src="2.png" className="d-block w-100" alt="slide1" />
+        <div className={`carousel-item ${index === 0 ? 'active' : null}`}>
+          <img src={Slide1} className="d-block w-100" alt="slide1" />
         </div>
-        <div className="carousel-item">
-          <img src="1.png" className="d-block w-100" alt="slide2" />
-        </div>
-        <div className="carousel-item">
-          <img src="3.png" className="d-block w-100" alt="slide3" />
+        <div className={`carousel-item ${index === 1 ? 'active' : null}`}>
+          <img src={Slide2} className="d-block w-100" alt="slide2" />
         </div>
       </div>
       <button
         className="carousel-control-prev"
         type="button"
-        data-bs-target="#carouselExample"
+        data-bs-target="#carousel"
         data-bs-slide="prev"
+        onClick={() => setSlide(index - 1)}
+        disabled={index === 0}
       >
         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
         <span className="visually-hidden">Previous</span>
@@ -24,8 +31,10 @@ export const Carousel = () => {
       <button
         className="carousel-control-next"
         type="button"
-        data-bs-target="#carouselExample"
+        data-bs-target="#carousel"
         data-bs-slide="next"
+        onClick={() => setSlide(index + 1)}
+        disabled={index === slides.length - 1}
       >
         <span className="carousel-control-next-icon" aria-hidden="true"></span>
         <span className="visually-hidden">Next</span>
