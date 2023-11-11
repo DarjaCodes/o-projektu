115748ddc034736a45d0ab9dc6a8faf95e0826fc
Kateřina Trnková <kat.trnkova@gmail.com>
2023-11-11 09:36:56 +0100

    zarovnání loga GH,úprava textu o projektu

--- src/pages/AboutProject/index.jsx
+++ src/pages/AboutProject/index.jsx
@@ -7,18 +7,19 @@ export const AboutProject = () => {
       <div className="text1">
         <div className="text1__container">
           <h2 className="text1__heading">O co jde?</h2>
+
           <p className="text1__about">
             Od začátku jsme chtěly pracovat na projektu, který by měl hlubší
-            smysl a sociální přesah. Dlouho jsme hledaly to "pravé", až do do
-            doby, kdy se Darja díky své práci v neziskovce dostala do kontaktu s
-            fotografkou Julií Dobryninovou, která zrovna plánovala udělat
-            fotografický projekt, který by poukazoval na problém ukrajinských
-            válečných uprchlic pracujících pod svou původní kvalifikaci. Juliin
-            nápad se nám moc líbil, a tak jsme se domluvily, že jí k němu
-            vytvoříme web, který bude sloužit i jako nástroj k osvětě. Zahájení
-            výstavy se plánuje na půlku prosince, takže máme co dělat, aby
-            návštěvníci po oskenování kódu neviděli na svých telefonech jen
-            smutný text s chybou 404..
+            smysl a sociální přesah. Dlouho jsme hledaly to "pravé", až do doby,
+            kdy se Darja díky své práci v neziskovce dostala do kontaktu s
+            fotografkou Julií Dobryninovou, která zrovna plánovala vytvořit
+            projekt, který by poukazoval na problém ukrajinských válečných
+            uprchlic pracujících pod svou původní kvalifikaci. Juliin nápad se
+            nám moc líbil, a tak jsme se domluvily, že jí k němu vytvoříme web,
+            který bude sloužit i jako nástroj k osvětě. Zahájení výstavy se
+            plánuje na půlku prosince, takže máme co dělat, aby návštěvníci po
+            oskenování QR kódu nespatřili na svých telefonech jen smutný text s
+            chybou 404..
           </p>
         </div>
       </div>
@@ -29,8 +30,8 @@ export const AboutProject = () => {
             Rády bychom se zaměřily na design a hezké zobrazování fotek pomocí
             animace. Web by měl být designován jako mobile first, jelikož
             předpokládáme, že díky QR kódu na výstavě si lidé budou náš web
-            otevírat především na svých telefonech. Naší ambicí je si vytvořit
-            lokální API, kam by sama fotografka mohla vkládat fotky na web.
+            otevírat především na svých telefonech. Naší ambicí je vytvořit
+            lokální API, kam by sama fotografka mohla vkládat své fotografie.
             Výstava bude zřejmě putovní, takže práce na webu Galavečerem
             rozhodně nekončí. Těšíme se na celý vývoj a na práci na reálné
             zakázce.
--- src/pages/AboutUs/index.jsx
+++ src/pages/AboutUs/index.jsx
@@ -9,23 +9,25 @@ export const AboutUs = () => {
       <div className="medajlonek1">
         <div className="darja">
           <h2 className="darja__nadpis">Darja Krjuková</h2>
-          <p className="about1">
-            Měla jsem být překladatelkou, ale osud mě zavál do marketingu a
-            následně i k webu. Moje cesta do IT začala jednoduchou dětskou
-            otázkou. Pracovala jsem několik let v digitálním marketingu a
-            ponořovala se do světa produktového obsahu, než jsem si nakonec
-            řekla: „A jak vlastně funguje ten web?“ Od té chvíle začalo pátrání,
-            neutichající zájem a nakonec i splnění snu - přijetí na Akademii.
-            Pořád mě to baví a mám silné tušení, že mě to jen tak nepustí.
-            Stejně tak jako joga, výhledy z kopců a především radost, když mám
-            po svém boku svoji dcerku.
-          </p>
-          <p className="github1">
-            GitHub
-            <a href="https://github.com/DarjaCodes">
-              <img className="github1__ref" src={Github} alt="logoGH"></img>
-            </a>
-          </p>
+          <div className="about1">
+            <p>
+              Měla jsem být překladatelkou, ale osud mě zavál do marketingu a
+              následně i k webu. Moje cesta do IT začala jednoduchou dětskou
+              otázkou. Pracovala jsem několik let v digitálním marketingu a
+              ponořovala se do světa produktového obsahu, než jsem si nakonec
+              řekla: „A jak vlastně funguje ten web?“ Od té chvíle začalo
+              pátrání, neutichající zájem a nakonec i splnění snu - přijetí na
+              Akademii. Pořád mě to baví a mám silné tušení, že mě to jen tak
+              nepustí. Stejně tak jako joga, výhledy z kopců a především radost,
+              když mám po svém boku svoji dcerku.
+            </p>
+            <p className="github1">
+              GitHub
+              <a href="https://github.com/DarjaCodes">
+                <img className="github1__ref" src={Github} alt="logoGH"></img>
+              </a>
+            </p>
+          </div>
         </div>
         <img src={Foto2} className="rounded-start-pill" alt="fotoKat"></img>
       </div>
@@ -33,26 +35,27 @@ export const AboutUs = () => {
         <img src={Foto1} className="rounded-end-pill" alt="FotoDarj"></img>
         <div className="katerina">
           <h2 className="katerina__nadpis">Kateřina Trnková</h2>
-
-          <p className="about2">
-            Jsem vystudovaná kulturoložka, která po studiích odjela za
-            dobrodružstvím do Brazílie. Když jsem se po třech letech vrátila
-            zpět do Prahy, tak jsem zjistila, že počet kulturologů na trhu práce
-            předčí o 100% poptávku po těchto profesionálech. Začala jsem tedy
-            pracovat v technologické firmě na zákaznickém servisu, kde jsem
-            vydržela doteď. Akademii jsem začala s nulovou znalostí IT, takže je
-            to pro mě velká výzva a téměř denodenní přísun kvanta nových
-            informací, jejichž zpracování trvá mému mozku déle, než by bylo
-            potřeba. Hlavu si nejraději větrám při běhání, čtení a procházkách
-            se svým psem. A samozřejmě jako každý správný mileniál, miluju
-            cestování 🌄
-          </p>
-          <p className="github2">
-            GitHub
-            <a href="https://github.com/KatTrnk">
-              <img className="github2__ref" src={Github} alt="logoGH"></img>
-            </a>
-          </p>
+          <div className="about2">
+            <p>
+              Jsem vystudovaná kulturoložka, která po studiích odjela za
+              dobrodružstvím do Brazílie. Když jsem se po třech letech vrátila
+              zpět do Prahy, tak jsem zjistila, že počet kulturologů na trhu
+              práce předčí o 100% poptávku po těchto profesionálech. Začala jsem
+              tedy pracovat v technologické firmě na zákaznickém servisu, kde
+              jsem vydržela doteď. Akademii jsem začala s nulovou znalostí IT,
+              takže je to pro mě velká výzva a téměř denodenní přísun kvanta
+              nových informací, jejichž zpracování trvá mému mozku déle, než by
+              bylo potřeba. Hlavu si nejraději větrám při běhání, čtení a
+              procházkách se psem. A samozřejmě jako každý správný mileniál
+              miluju cestování 🌄
+            </p>
+            <p className="github2">
+              GitHub
+              <a href="https://github.com/KatTrnk">
+                <img className="github2__ref" src={Github} alt="logoGH"></img>
+              </a>
+            </p>
+          </div>
         </div>
       </div>
     </div>
